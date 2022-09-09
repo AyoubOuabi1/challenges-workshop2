945a263b13b6804b20f064f85abfaa7ee9b0d777
AyoubOuabi1 <ayoub.ouabi06@gmail.com>
2022-09-09 11:56:51 +0100

    last version

--- Challenge 1  Paire ou Impaire/main.c
+++ Challenge 1  Paire ou Impaire/main.c
@@ -3,20 +3,15 @@
 
 int main() {
     // Write C code here
-   // nt nmb;
+    int nmb;
 
-    //printf("saisir un nomber \n");
-    //scanf("%d",&nmb);
-    //if(nmb%2==0){
-    //	printf("paire ");
-	//}else {
-	//	printf("impaire ");
-	//}
-//	float c=(float) 5/3;
-//	printf("%f",(float) 5/3);
-	int i;
-	printf("nomber \n");
-	scanf("%d",&i);
-	printf("H %x :",i);
+    printf("saisir un nomber \n");
+    scanf("%d",&nmb);
+    if(nmb%2==0){
+    	printf("paire ");
+	}else {
+		printf("impaire ");
+	}
+
     return 0;
 }
