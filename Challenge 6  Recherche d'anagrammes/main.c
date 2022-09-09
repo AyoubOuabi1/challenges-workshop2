e90e9cead4c2576f2e325857df767c2321dbead1
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2022-09-09 11:59:36 +0100

    Update main.c

--- Challenge 6  Recherche d'anagrammes/main.c
+++ Challenge 6  Recherche d'anagrammes/main.c
@@ -2,23 +2,57 @@
  
 int main() {
     // Write C code here
-    char m1[100]="abc d e ",m2[100]=" ea db c";
-    char m3[100];
-    char arr[100][100];
-    int i,j;
+    char m1[4];
+    char m2[4];
+    int c,i;
     printf("saisir la 1er chaines de caracteres \n");
-    scanf("%[^\n]%*c",&m1);
+    scanf("%s",&m1);
     printf("saisir la 2eme chaines de caracteres \n");
-    scanf("%[^\n]%*c",&m2);
+    scanf("%s",&m2);
+    
+    for(i=0;i<4;i++){
+    	if(m1[i]==m2[i]){
+    		c++;
+		}
+		else{
+			break;
+		}
+	}
+	if(c==4){
+		printf("Anagramme");
+	}else {
+		printf("Pas d'anagramme !");
+	}
+    /*printf("saisir la taille de la  1er chaines de caracteres \n");
+    scanf("%d",&t1);
+    printf("saisir la taille de la 2eme chaines de caracteres \n");
+    scanf("%d",&t2);
+    
+    char m1[t1];
+    char m2[t2];
+    char arr[t1+t2];
+    int i,j;
+    for (i=0;i<sizeof(m1),i++){
+    	printf("\n saisir la caracter nomber : %d ",i);
+    	scanf("%c",arr[i]);
+	}
+	for (i=0;i<sizeof(m1),i++){
+    	printf("\n saisir la caracter nomber : %d \n",i);
+    	scanf("%c",arr[i]);
+	}
+  
+    
     for(i=0;i<100;i++){
     	for(j=0;j<100;j++){
     		if(m1[i]==m2[j]){
     			arr[i][j]=[i][j];
 			}else{
 				printf("f");
+				break
+				
 			}
 		}
-	}
+	}*/
  	//printf("%s,%s",m1,m2);
  	
     return 0;
