370cb116b1d83cf33b076a9ae057310c7bec0abc
AyoubOuabi1 <ayoub.ouabi06@gmail.com>
2022-09-12 11:05:22 +0100

    challenge 10 une fonction pour verifie le rest de la division A par B est un nomber premier ou non

---
+++ Challenge 10/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h> 
+bool divededby(int a,int b);
+bool isPremier(int a);
+
+int main(){
+	
+	int a,b;
+	printf("saisir un nomber A \n ");
+	scanf("%d",&a);
+	
+	printf("saisir un nomber B \n ");
+	scanf("%d",&b);
+	
+	if (divededby(a,b)){
+		printf("le reste de la division A  par B est  un nomber premier ");
+	}else {
+		printf("le reste de la division A  par B est pas un nomber premier");
+	}
+}
+
+bool divededby(int a,int b){
+	int m=a%b;
+ 	if(isPremier(m)){
+		return true;
+	}else {
+		return false;
+	}
+ 
+	
+	
+}
+bool isPremier(int a){
+	int i;
+
+	if(a!=1){
+		for(i=2;i<a;i++){
+			if(a%i==0){
+				return false;
+			}
+		}	
+	}
+	return true;
+	
+}
