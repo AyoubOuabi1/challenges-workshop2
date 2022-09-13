543b38f459b240ed108a80e4948d24466060f02b
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2022-09-13 13:53:41 +0100

    correction de la cmnt

--- Challenge  8/main.c
+++ Challenge  8/main.c
@@ -8,7 +8,7 @@ int main(){
 	
 	printf("saisir la nomber B : \n");
 	scanf("%d",&b);
-	//Supprimez la valuer d'origin de A et remplacez-la par la valeur de A et Supprimez la valuer d'origin de A et remplacez-la par la valeur de B
+	//Supprimez la valuer d'origin de A et remplacez-la par la valeur de B et Supprimez la valuer d'origin de B et remplacez-la par la valeur de A
 	echanger(a,b);
 }
 int echanger(int a,int b){
