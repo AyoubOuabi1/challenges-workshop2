b9b7240146179d4024a76a4aa67b459c2b5c5aec
Ayoub Ouabi <112890204+AyoubOuabi1@users.noreply.github.com>
2022-09-13 09:15:58 +0100

    correction

--- Challenge  8/main.c
+++ Challenge  8/main.c
@@ -8,9 +8,14 @@ int main(){
 	
 	printf("saisir la nomber B : \n");
 	scanf("%d",&b);
-	//Supprimez la valuer d'origin de A et remplacez-la par la valeur de b
-	printf("la valuer  de A est : %d",echanger(a,b));
+	//Supprimez la valuer d'origin de A et remplacez-la par la valeur de A et Supprimez la valuer d'origin de A et remplacez-la par la valeur de B
+	echanger(a,b);
 }
 int echanger(int a,int b){
-	return a=b;
+	int c;
+	c=a;
+	a=b;
+	b=c;
+	printf("la valuer  de A est : %d,et la valuer  de B est : %d",a,b);
+	return 0;
 }
